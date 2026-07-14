d87dd8a7aaa7cf521ad95ce817994909687c2d4a
abhishekkp00 <abhishekforcollege@gmail.com>
2026-07-14 13:47:37 +0530

    feat: Node/Edge representation for visualizers

---
+++ src/NV.java
@@ -0,0 +1,13 @@
+public class NV {
+    public int id;
+    public String metadata;
+    public String category;
+    public int maxLyr;
+
+    public NV(int id, String metadata, String category, int maxLyr) {
+        this.id = id;
+        this.metadata = metadata;
+        this.category = category;
+        this.maxLyr = maxLyr;
+    }
+}
