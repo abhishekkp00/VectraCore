cb17ebe6002e3885e9f5a9bdb2e765f5e5af95cc
abhishekkp00 <abhishekforcollege@gmail.com>
2026-07-14 13:50:18 +0530

    feat: Visuals

---
+++ src/EV.java
@@ -0,0 +1,11 @@
+public class EV {
+    public int src;
+    public int dst;
+    public int lyr;
+
+    public EV(int src, int dst, int lyr) {
+        this.src = src;
+        this.dst = dst;
+        this.lyr = lyr;
+    }
+}
